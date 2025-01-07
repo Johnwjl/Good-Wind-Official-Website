a3b10570d69a5bc9bc7228d3b50385991f6641ee
Jello <johnwangjl@gmail.com>
2025-01-07 16:36:43 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  ui: {
+    fonts: false
+  },
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: "2024-11-27"
+})
